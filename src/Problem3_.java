ab58817981b029c29741eb4310b34246c4b5a2bd
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-08 22:55:23 +0600

    Update and rename Second_problem.java to Problem3_.java

--- src/Problem3_.java
+++ src/Problem3_.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Second_problem {
+public class Problem3_ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
